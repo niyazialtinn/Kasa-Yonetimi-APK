8508e2cbe88edfd379be0c2ce1bc5e7bb6b7c559
niyazialtinn <niyazi.altin1@gmail.com>
2026-09-24 15:25:12 +0300

    Create MainActivity.kt

---
+++ app/src/main/java/com/niyazi/kasayonetimi/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.niyazi.kasayonetimi
+
+import android.app.Activity
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this)
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+
+        webView.loadUrl("file:///android_asset/index.html")
+
+        setContentView(webView)
+    }
+}
